28b5d4d72b8782dd949f08457ee698369666d9e8
Nicholas Barham <nickkeysgaming@gmail.com>
2020-07-25 17:15:11 +0100

    Counter class added

---
+++ Assets/Scripts/Counter/ICounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pong
+{
+    public interface ICounter
+    {
+        int CurrentCount { get; }
+        int MaxCount { get; }
+        event Action<int> OnCountChanged;
+        event Action OnMaxCountReached;
+        void Increment();
+        void Decrement();
+        void Reset();
+    }
+}
