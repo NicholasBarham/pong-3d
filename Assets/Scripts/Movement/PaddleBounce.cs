c03967f98541e8f836d0ef15a1688c84346d6d67
Nicholas Barham <nickkeysgaming@gmail.com>
2020-07-26 23:38:52 +0100

    Prototype ball movement with set angles created

---
+++ Assets/Scripts/Movement/PaddleBounce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PaddleBounce : MonoBehaviour
+{
+    BoxCollider paddleCollider = null;
+
+    private void Awake()
+    {
+        paddleCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float pointX = collision.GetContact(0).point.x;
+        Vector3 localPositionOfContact = transform.InverseTransformPoint(pointX, 0f, 0f);
+
+        Rigidbody ballRigidbody = collision.collider.attachedRigidbody;
+
+        Vector3 ballVelocity = ballRigidbody.velocity;
+        float ballSpeed = ballVelocity.magnitude;
+
+        float paddleXExtent = paddleCollider.bounds.extents.x;
+
+        float xPercentage = (localPositionOfContact.x / paddleXExtent) - 1f;
+
+        ballRigidbody.velocity = new Vector3(xPercentage, 0f, 1f) * ballSpeed;
+    }
+}
